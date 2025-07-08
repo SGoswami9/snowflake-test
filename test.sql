2459e8997d7b60567aca2a62c68902514d5b3dd6
SGoswami9 <91060691+SGoswami9@users.noreply.github.com>
2025-07-08 16:51:01 +0100

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,6 +1,10 @@
 select current_timestamp();
 select current_session();
 
-set database_names = ['DB1','DB2'];
+SET DATABASE_NAMES = ['DB1','DB2'];
 
-CREATE DATABASE IF NOT EXISTS $database_names[0];
+for db_name in database_names
+  'CREATE DATABASE IF NOT EXISTS' || database_names ;
+end for;
+
+
